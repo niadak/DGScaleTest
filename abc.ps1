334b67f57a47382577951dbe000774d5a69e6892
Niadak <niadak@microsoft.com>
2018-03-09 13:03:04 +0530

    Create abc.ps1

---
+++ abc.ps1
@@ -0,0 +1,5 @@
+
+param([string]$agentDownloadUrl)
+
+Write-Verbose -Verbose 'Test script executed successfully. parameter passed $agentDownloadUrl'
+Write-Host 'Test script executed successfully. parameter passed $agentDownloadUrl'
